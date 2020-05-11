dd654260dd58c57362c966b9a6d840812cd8f307
sid24gupta <47803520+sid24gupta@users.noreply.github.com>
2020-05-11 12:23:10 +0530

    Update settings.py

--- stockyard/settings.py
+++ stockyard/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'main/static'),
-)
+STATIC_ROOT = 'D:/stockyard/main/static'
+
 
 
 """DEFAULT_FILE_STORAGE = 'main.custom_azure.AzureMediaStorage'
